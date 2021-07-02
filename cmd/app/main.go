9e89a5cb23126a4a74ca69c8d3fa6854c5dcd112
Renat Abaidulin <ares566@ya.ru>
2021-07-02 15:26:14 +0300

    Реорганизовал забор остатков на складе у товара

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,18 +6,16 @@ import (
 
 func main() {
 
-	productURL := []string{
-		"https://market.yandex.ru/product--weleda-maslo-dlia-mladentsev-s-kalenduloi-s-nezhnym-aromatom/1722845046?nid=18044746&show-uid=16250239923955073468616003&context=search&glfilter=7893318%3A8513199&onstock=1&sku=100242020836&cpc=Yx9sPGU0iDU7srEKm7Q9niTzkEBn8l2H9XQzDp0-XZvZyuN9sGwr93EAFakavWsPknbiMgPlV5kP66oHh696GhaBqpirMeknp60A1tmpaMD2otpS9hRRDV0QpG4VRshFWzClueCC9AHXZngPwpRt5eSd93ajwACHficVcA4IfoWw05tQlbBb9A%2C%2C&do-waremd5=MM5ZEPEDe0pFL2iJNNnGTQ",
-		"https://market.yandex.ru/product--weleda-detskoe-molochko-dlia-giperchuvstvitelnoi-kozhi-tela-s-alteem/1722845051?nid=18044746&show-uid=16250239923955073468616004&context=search&glfilter=7893318%3A8513199&onstock=1&sku=100318425259&cpc=Yx9sPGU0iDVfmcrLvxRn7_kea6YJUHAmdoN9Glw9gAgrzs_52ajQLBGQJ2i7Lnx156JE2Ts7tbzXBHaUZRJFnfNc3QVo74Q6AiEQpRJLrtiadENmDXNsQpunyQ5EPmFvG-FBx0nKWUJplZFw-BW_UAFFZm4BB-5i0iusYByRFk8_q2is3eI9Fw%2C%2C&do-waremd5=Wesl8risSlBVIQeOcQUHug",
-		"https://market.yandex.ru/product--weleda-detskii-balzam-zashchitnyi-ot-vetra-i-kholoda-s-kalenduloi/1722845037?nid=18044746&show-uid=16250239923955073468616009&context=search&glfilter=7893318%3A8513199&onstock=1&sku=100234383551&cpc=uaV08jcA5Qbk4txyDVQcJRFQ2dTNLzWLld_I4n69z0Jq1QC1o_RK5dTmUgD_vk31FrpQWHoNg1dS_CWVxVI5PTCs9nmRROKkDZLmF-IAtQK4tPeZaj9BGnGmdtfgBJi-RPzw8SWsez6e1A5w2d0C2Z6JAzLazubNc3KP4j5yQzJyV7Rpdlsicg%2C%2C&do-waremd5=n2JLc8YVR23LxNrTklrFIQ",
+	productURLs := []string{
+		"https://market.yandex.ru/product--weleda-detskoe-molochko-dlia-giperchuvstvitelnoi-kozhi-tela-s-alteem/1722845051?nid=18044746&show-uid=16252283348025817253216004&context=search&glfilter=7893318%3A8513199&onstock=1&sku=100318425259&cpc=duAezd3xFTIcEnogw76L7iR_8OU-Rnayud9IefH2Wt1YLroQMpjVuv2D19Cu4te1zXfddaxjigPOQA1th-QdP3WV9GUvT72zW0qUUw50PJdJpLqB0kxiJjXwgk8WM6TTms3YQRKU9MHsEDo4Wg92IGnKlykY7IKco5iKNALdPXz4HFQbhi8hWQ%2C%2C&do-waremd5=Wesl8risSlBVIQeOcQUHug",
 	}
 
 	// парсим данные товара url
-	ProductScrapper := scraper.NewProductScraper(productURL)
+	//ProductScrapper := scraper.NewProductScraper(productURLs)
 
 	// парсим остатки на складе для товара заданного url
-	//Scraper := scraper.NewBasketScraper(url)
+	BasketScraper := scraper.NewBasketScraper(productURLs)
 
-	scraper.NewClient(ProductScrapper)
+	scraper.NewClient(BasketScraper)
 
 }
--- internal/scraper/basket.go
+++ internal/scraper/basket.go
@@ -3,89 +3,94 @@ package scraper
 import (
 	"context"
 	"fmt"
-	"log"
+	"math/rand"
 
 	"time"
 
-	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 )
 
-type Basket struct {
-	url          string // это не URL корзинки, это URL товара который нужно положить в корзину
-	stockBalance string
-	bcrumbs      []*cdp.Node
-	name         string
-	label        string
-	mark         string
-	rpl          string
-	oldPrice     string
-	discount     string
-	curPrice     string
+type ProductBalance struct {
+	Url     string
+	Balance string
 }
 
-func NewBasketScraper(_url string) *Basket {
-	return &Basket{url: _url}
+type BasketScraiper struct {
+	urls     []string
+	Balances []ProductBalance
 }
 
-func (basket *Basket) process(ctx context.Context) {
+func NewBasketScraper(_urls []string) *BasketScraiper {
+	return &BasketScraiper{urls: _urls}
+}
+
+func (basketscraper *BasketScraiper) process(ctx context.Context) {
 
 	basketCnt := "1000"
 
-	err := chromedp.Run(
-		ctx,
+	rand.Seed(time.Now().UnixNano())
 
-		chromedp.Navigate(basket.url),
-		chromedp.Sleep(500*time.Millisecond),
-		chromedp.WaitVisible("div.x__tNeZtug", chromedp.ByQuery),
-		chromedp.Sleep(7000*time.Millisecond),
+	for _, producturl := range basketscraper.urls {
 
-		// берем данные товара, раз уж загрузили
-		chromedp.Nodes(`//span[@itemprop='name']/text()`, &basket.bcrumbs),
-		chromedp.InnerHTML(`//div[@class='x__tNeZtug']/div/h1`, &basket.name),
-		chromedp.InnerHTML(`//div[@class='QldO3ndWeZ']`, &basket.label),
-		chromedp.InnerHTML(`//span[@class='_3nFvoU2Uov']`, &basket.mark),
-		chromedp.InnerHTML(`//a[@class='_27nuSZ19h7 _2J5l3Ahj0X _15qW8ohHT4 cia-cs']`, &basket.rpl),
-		chromedp.InnerHTML(`//div[@class="h2EBR70V3s _34FT3corIW"]/span[@class='_5cNkUrAI2M']`, &basket.oldPrice),
-		chromedp.InnerHTML(`//div[@class='_1xSlMTtCCt _1emLjCV7YT']/span[@class='_1L-3GDwnCL']`, &basket.discount),
-		chromedp.InnerHTML(`//div[@class='_3NaXxl-HYN _3kWlKUNlTg _3WgcBT2Eyd']/span/span[1]`, &basket.curPrice),
+		// задержка для совести
+		randomSleep := 5 + rand.Intn(20)
+		time.Sleep(time.Duration(randomSleep) * time.Second)
 
-		// добавляем в корзину товар
-		chromedp.Click(`//button[@class='zsSJkfeAPw _16jABpOZ2- LS3-2-cZ2Z _2Sz75Y384m _3iugb2hKWO _2VlTHnWxF8 _1jnLi6H271']`),
-		chromedp.Sleep(2000*time.Millisecond),
+		balance := ProductBalance{Url: producturl}
 
-		// переходим в корзину
-		chromedp.Navigate("https://pokupki.market.yandex.ru/my/cart"),
-		chromedp.Sleep(5000*time.Millisecond),
-		chromedp.WaitVisible("div.b_3bUkPPXxr5", chromedp.ByQuery),
-		chromedp.Sleep(2000*time.Millisecond),
+		// ждем загрузки страницы
+		err := chromedp.Run(
+			ctx,
+			RunWithTimeOut(&ctx, 20, chromedp.Tasks{
+				chromedp.Navigate(balance.Url),
+				chromedp.Sleep(555 * time.Millisecond),
+				chromedp.WaitVisible("div.x__tNeZtug", chromedp.ByQuery),
 
-		// увеличиваем счетчик товаров до 10000
-		chromedp.SendKeys(`//div[@class='b_3I6VbdL-46']//input[@class='b_3qxDpnyrNf']`, basketCnt),
-		chromedp.Sleep(3000*time.Millisecond),
+				// добавляем в корзину товар
+				chromedp.Click(`//button[@class='zsSJkfeAPw _16jABpOZ2- LS3-2-cZ2Z _2Sz75Y384m _3iugb2hKWO _2VlTHnWxF8 _1jnLi6H271']`),
+				chromedp.Sleep(2000 * time.Millisecond),
+			}),
+		)
 
-		// проверяем остатки на складе
-		chromedp.InnerHTML(`//div[@class='b_iVxPKotZvY']//div[@class='cia-vs']`, &basket.stockBalance),
-	)
+		// TODO нужно добавить лог ошибок и его анализатор чтоб поймать момент когда парсинг уже в принципе не работает
+		if err != nil && err != context.Canceled && err != context.DeadlineExceeded {
+			fmt.Printf("%s", err)
+			continue
+		}
 
-	fmt.Printf("URL: %s\n", basket.url)
-	fmt.Print("Breadcrumbs: ")
-	for _, item := range basket.bcrumbs {
-		fmt.Print(" ► ")
-		fmt.Print(item.NodeValue)
+		err = chromedp.Run(
+			ctx,
+			RunWithTimeOut(&ctx, 20, chromedp.Tasks{
+				// переходим в корзину
+				chromedp.Navigate("https://pokupki.market.yandex.ru/my/cart"),
+				chromedp.Sleep(5000 * time.Millisecond),
+				chromedp.WaitVisible("div.b_3bUkPPXxr5", chromedp.ByQuery),
+				chromedp.Sleep(2000 * time.Millisecond),
+
+				// увеличиваем счетчик товаров до 10000
+				chromedp.SendKeys(`//div[@class='b_3I6VbdL-46']//input[@class='b_3qxDpnyrNf']`, basketCnt),
+				chromedp.Sleep(3000 * time.Millisecond),
+
+				// проверяем остатки на складе
+				chromedp.InnerHTML(`//div[@class='b_iVxPKotZvY']//div[@class='cia-vs']`, &balance.Balance),
+			}),
+		)
+
+		if err != nil && err != context.Canceled && err != context.DeadlineExceeded {
+			fmt.Printf("%s", err)
+			continue
+		}
+
+		// TODO добавить условия достаточности информации для вставки в БД
+		basketscraper.Balances = append(basketscraper.Balances, balance)
+
 	}
-	fmt.Println("")
-	fmt.Printf("Название: %s\n", basket.name)
-	fmt.Printf("Метка: %s\n", basket.label)
-	fmt.Printf("Рейтинг: %s\n", basket.mark)
-	fmt.Printf("Отзывы: %s\n", basket.rpl)
-	fmt.Printf("Цена без скидки: %s\n", basket.oldPrice)
-	fmt.Printf("Цена со скидкой: %s\n", basket.curPrice)
-	fmt.Printf("Дисконт: %s\n", basket.discount)
-	fmt.Printf("Остатки на складе: %s \n", basket.stockBalance)
 
-	if err != nil {
-		log.Fatal(err)
+	for _, balance := range basketscraper.Balances {
+
+		fmt.Printf("URL: %s\n", balance.Url)
+		fmt.Printf("Остаток: %s\n", balance.Balance)
+
 	}
 
 }
